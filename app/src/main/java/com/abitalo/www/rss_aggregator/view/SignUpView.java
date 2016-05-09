aa293f2f0b8b3007d87ae8ae3f0ff86835b51b17
sangzhenya <sangzhenya@gmail.com>
2016-05-09 16:37:48 +0800

    增加了登陆界面、注册界面和找回密码界面，完成了登陆和注册界面的交互逻辑

---
+++ app/src/main/java/com/abitalo/www/rss_aggregator/view/SignUpView.java
@@ -0,0 +1,122 @@
+package com.abitalo.www.rss_aggregator.view;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.widget.DrawerLayout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.abitalo.www.rss_aggregator.R;
+import com.abitalo.www.rss_aggregator.presenter.AccountPresenter;
+import com.abitalo.www.rss_aggregator.util.MD5Encrypt;
+
+/**
+ * Created by sangzhenya on 2016/5/8.
+ * 注册界面
+ */
+public class SignUpView extends Fragment implements View.OnClickListener, IAccountView {
+    AccountPresenter presenter = null;
+    DrawerLayout drawer = null;
+    View view = null;
+
+    EditText etFullName = null;
+    EditText etEmail = null;
+    EditText etPassword = null;
+    EditText etConfirmPassword = null;
+
+    Button btnCreateAccount = null;
+    TextView tvExist2Login = null;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.sign_up_main, container, false);
+        initView();
+        return view;
+    }
+
+    private void initView() {
+        etFullName = (EditText) view.findViewById(R.id.sign_full_name);
+        etEmail = (EditText) view.findViewById(R.id.sign_user_mail);
+        etPassword = (EditText) view.findViewById(R.id.sign_user_pass);
+        etConfirmPassword = (EditText) view.findViewById(R.id.sign_up_edit_confirm_password);
+
+        btnCreateAccount = (Button) view.findViewById(R.id.sign_login);
+
+        tvExist2Login = (TextView) view.findViewById(R.id.sign_exist);
+
+        btnCreateAccount.setOnClickListener(this);
+        tvExist2Login.setOnClickListener(this);
+
+        drawer = (DrawerLayout) getActivity().findViewById(R.id.drawer_layout);
+        presenter = new AccountPresenter(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.sign_login:
+                create();
+                break;
+            case R.id.sign_exist:
+                signIn();
+                break;
+        }
+    }
+
+    private void signIn() {
+        FragmentManager fragmentManager = getFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.fragment_content, new SignInView(), "sign_in").commit();
+    }
+
+    private void create() {
+        if (etPassword.getText().toString().equals(etConfirmPassword.getText().toString())) {
+            presenter.register();
+        } else {
+            Snackbar.make(view, "Enter the password twice inconsistent!!", Snackbar.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public String getUserName() {
+        return etEmail.getText().toString();
+    }
+
+    @Override
+    public String getPassword() {
+        return MD5Encrypt.parse(etPassword.getText().toString());
+    }
+
+    @Override
+    public boolean onFailure(String msg) {
+        Snackbar.make(view, msg, Snackbar.LENGTH_SHORT).show();
+        return false;
+    }
+
+    @Override
+    public boolean onSuccess() {
+        showMenu();
+        Snackbar.make(view, "Success!", Snackbar.LENGTH_SHORT).show();
+        return false;
+    }
+
+    private void showMenu() {//将来需要使用从云端获得的用户数据作为参数，然后使用adapter根据用户数据填充菜单。。
+        NavigationView navigationView = (NavigationView) getActivity().findViewById(R.id.nav_account);
+        navigationView.addView(new AccountNavigationView(getActivity()));
+        onDestroy();
+        drawer.openDrawer(getActivity().findViewById(R.id.nav_account));
+
+        FragmentManager fragmentManager = getFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.fragment_content, new MainContentDemo(), "Main content").commit();
+    }
+}
--- app/src/main/java/com/abitalo/www/rss_aggregator/view/WelcomeNav.java
+++ app/src/main/java/com/abitalo/www/rss_aggregator/view/WelcomeNav.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.widget.DrawerLayout;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,45 +20,52 @@ import com.abitalo.www.rss_aggregator.util.MD5Encrypt;
 
 /**
  * Created by Lancelot on 2016/5/3.
+ * 左划拉出的欢迎界面
  */
-public class WelcomeNav extends Fragment implements View.OnClickListener,IAccountView{
-    AccountPresenter presenter=null;
-
+public class WelcomeNav extends Fragment implements View.OnClickListener {
     View view = null;
-    EditText username=null;
-    EditText password=null;
-    Button btnLoginSubmit = null;
-    Button btnRegisterSubmit=null;
+    Button btnLogin = null;
+    Button btnAddContent = null;
+    DrawerLayout drawer = null;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        view = inflater.inflate(R.layout.nav_account,container,false);
+        view = inflater.inflate(R.layout.nav_welcome, container, false);
         initialView();
         return view;
     }
 
     private void initialView(){
-        username=(EditText) view.findViewById(R.id.edit_login_username);
-        password=(EditText) view.findViewById(R.id.edit_login_password);
-
-        btnLoginSubmit=(Button) view.findViewById(R.id.btn_login_submit);
-        btnLoginSubmit.setOnClickListener(this);
-
-        btnRegisterSubmit=(Button)view.findViewById(R.id.btn_login_register);
-        btnRegisterSubmit.setOnClickListener(this);
+        btnLogin = (Button) view.findViewById(R.id.welcomeLogin);
+        btnAddContent = (Button) view.findViewById(R.id.addContent);
+        btnLogin.setOnClickListener(this);
+        btnAddContent.setOnClickListener(this);
+        drawer = (DrawerLayout) getActivity().findViewById(R.id.drawer_layout);
     }
 
     @Override
     public void onClick(View v) {
-        if(v.getId()==R.id.btn_login_submit){
-            presenter=new AccountPresenter(this);
-            presenter.login();
-        }else if(v.getId()==R.id.btn_login_register){
-            presenter=new AccountPresenter(this);
-            presenter.register();
+        switch (v.getId()) {
+            case R.id.welcomeLogin:
+                gotoLoginPage();
+                break;
+            case R.id.addContent:
+                addContent();
+                break;
         }
     }
 
-    private void showMenu(){//将来需要使用从云端获得的用户数据作为参数，然后使用adapter根据用户数据填充菜单。。
+    private void addContent() {
+        Log.i("welcome", "here you are");
+        drawer.closeDrawer(getActivity().findViewById(R.id.nav_account));
+        drawer.openDrawer(getActivity().findViewById(R.id.discovery_nav_view));
+    }
+
+    private void gotoLoginPage() {
+        FragmentManager fragmentManager = getFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.fragment_content, new SignInView(), "sign_in").commit();
+        drawer.closeDrawer(getActivity().findViewById(R.id.nav_account));
+    }
+    /*private void showMenu(){//将来需要使用从云端获得的用户数据作为参数，然后使用adapter根据用户数据填充菜单。。
         NavigationView navigationView=(NavigationView)getActivity().findViewById(R.id.nav_account);
         navigationView.addView(new AccountNavigationView(getActivity()));
         onDestroy();
@@ -88,5 +98,5 @@ public class WelcomeNav extends Fragment implements View.OnClickListener,IAccoun
     @Override
     public Context getContext() {
         return super.getContext();
-    }
+    }*/
 }
